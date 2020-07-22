faa2a1dbc664a7fee7e49b55b135d0e589652131
Max Ufer <maxufer@live.de>
2020-07-22 14:49:14 +0200

    Added reading of session IDs for wolfssl, but is untested, as wikipedia doesnt create one, and google doesnt work bc of tls13

--- agent/wolfssl.ts
+++ agent/wolfssl.ts
@@ -2,14 +2,14 @@ import { readAddresses, getPortsAndAddresses } from "./shared"
 
 export function execute() {
     var library_method_mapping: { [key: string]: Array<String> } = {}
-    library_method_mapping["*libwolfssl*"] = ["wolfSSL_read", "wolfSSL_write", "wolfSSL_get_fd"] //, "wolfSSL_get_session", "wolfSSL_SESSION_get_id", "wolfSSL_new", "wolfSSL_CTX_set_keylog_callback", "SSL_get_SSL_CTX"]
+    library_method_mapping["*libwolfssl*"] = ["wolfSSL_read", "wolfSSL_write", "wolfSSL_get_fd", "wolfSSL_get_session"] //, "wolfSSL_SESSION_get_id", "wolfSSL_new", "wolfSSL_CTX_set_keylog_callback", "SSL_get_SSL_CTX"]
     library_method_mapping["*libc*"] = ["getpeername", "getsockname", "ntohs", "ntohl"]
 
     var addresses: { [key: string]: NativePointer } = readAddresses(library_method_mapping)
 
     var SSL_get_fd = new NativeFunction(addresses["wolfSSL_get_fd"], "int", ["pointer"])
-    /*
     var SSL_get_session = new NativeFunction(addresses["wolfSSL_get_session"], "pointer", ["pointer"])
+    /*
     var SSL_SESSION_get_id = new NativeFunction(addresses["wolfSSL_SESSION_get_id"], "pointer", ["pointer", "pointer"])
     var SSL_CTX_set_keylog_callback = new NativeFunction(addresses["wolfSSL_CTX_set_keylog_callback"], "void", ["pointer", "pointer"])
     */
@@ -21,7 +21,7 @@ export function execute() {
        *     SSL_SESSION. For example,
        *     "59FD71B7B90202F359D89E66AE4E61247954E28431F6C6AC46625D472FF76336".
        */
-    /*
+
     function getSslSessionId(ssl: NativePointer) {
         var session = SSL_get_session(ssl) as NativePointer
         if (session.isNull()) {
@@ -29,7 +29,7 @@ export function execute() {
             return 0
         }
         var len_pointer = Memory.alloc(4)
-        var p = SSL_SESSION_get_id(session, len_pointer) as NativePointer
+        var p = session.add(8);
         var len = len_pointer.readU32()
         var session_id = ""
         for (var i = 0; i < len; i++) {
@@ -41,13 +41,12 @@ export function execute() {
         }
         return session_id
     }
-    */
+
     Interceptor.attach(addresses["wolfSSL_read"],
         {
             onEnter: function (args: any) {
                 var message = getPortsAndAddresses(SSL_get_fd(args[0]) as number, true, addresses)
-                //message["ssl_session_id"] = getSslSessionId(args[0])
-                message["ssl_session_id"] = ""
+                message["ssl_session_id"] = getSslSessionId(args[0])
                 message["function"] = "wolfSSL_read"
                 this.message = message
                 this.buf = args[1]
@@ -65,8 +64,7 @@ export function execute() {
         {
             onEnter: function (args: any) {
                 var message = getPortsAndAddresses(SSL_get_fd(args[0]) as number, false, addresses)
-                //message["ssl_session_id"] = getSslSessionId(args[0])
-                message["ssl_session_id"] = ""
+                message["ssl_session_id"] = getSslSessionId(args[0])
                 message["function"] = "wolfSSL_write"
                 message["contentType"] = "datalog"
                 send(message, args[1].readByteArray(parseInt(args[2])))
