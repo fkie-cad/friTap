a15d07367541bd22d6b916a6d3d4ff8b4cd959cb
Max Ufer <maxufer@live.de>
2021-02-19 10:38:43 +0100

    Formatting

--- friTap.py
+++ friTap.py
@@ -24,24 +24,26 @@ ssl_sessions = {}
 
 
 filename = ""
-tmpdir=""
+tmpdir = ""
 
 # Names of all supported read functions:
-SSL_READ = ["SSL_read", "wolfSSL_read", "readApplicationData","NSS_read"]
+SSL_READ = ["SSL_read", "wolfSSL_read", "readApplicationData", "NSS_read"]
 # Names of all supported write functions:
-SSL_WRITE = ["SSL_write", "wolfSSL_write", "writeApplicationData","NSS_write"]
+SSL_WRITE = ["SSL_write", "wolfSSL_write", "writeApplicationData", "NSS_write"]
+
 
 def write_pcap_header(pcap_file):
     for writes in (
-            ("=I", 0xa1b2c3d4),     # Magic number
-            ("=H", 2),              # Major version number
-            ("=H", 4),              # Minor version number
-            ("=i", time.timezone),  # GMT to local correction
-            ("=I", 0),              # Accuracy of timestamps
-            ("=I", 65535),          # Max length of captured packets
-                ("=I", 101)):           # Data link type (LINKTYPE_IPV4 = 228) CHANGED TO RAW
-            pcap_file.write(struct.pack(writes[0], writes[1]))
+        ("=I", 0xa1b2c3d4),     # Magic number
+        ("=H", 2),              # Major version number
+        ("=H", 4),              # Minor version number
+        ("=i", time.timezone),  # GMT to local correction
+        ("=I", 0),              # Accuracy of timestamps
+        ("=I", 65535),          # Max length of captured packets
+            ("=I", 101)):           # Data link type (LINKTYPE_IPV4 = 228) CHANGED TO RAW
+        pcap_file.write(struct.pack(writes[0], writes[1]))
     return pcap_file
+
 
 def cleanup(live=False):
     if live:
@@ -49,7 +51,6 @@ def cleanup(live=False):
         os.rmdir(tmpdir)  # Remove directory
     print("\nThx for using friTap\nHave a nice day\n")
     os._exit(0)
-    
 
 
 def temp_fifo():
@@ -61,8 +62,7 @@ def temp_fifo():
     try:
         return filename
     except OSError as e:
-        print(f'Failed to create FIFO: {e}') 
-
+        print(f'Failed to create FIFO: {e}')
 
 
 def ssl_log(app, pcap=None, verbose=False, spawn=False, keylog=False, enable_spawn_gating=False, android=False, live=False):
@@ -138,7 +138,7 @@ def ssl_log(app, pcap=None, verbose=False, spawn=False, keylog=False, enable_spa
                 (">H", 0x5018),                   # Header Length and Flags
                 (">H", 0xFFFF),                   # Window Size
                 (">H", 0),                        # Checksum
-                (">H", 0)):                       # Urgent Pointer
+                    (">H", 0)):                       # Urgent Pointer
                 pcap_file.write(struct.pack(writes[0], writes[1]))
 
             pcap_file.write(data)
@@ -179,7 +179,6 @@ def ssl_log(app, pcap=None, verbose=False, spawn=False, keylog=False, enable_spa
                 pcap_file.write(struct.pack(writes[0], writes[1]))
 
             pcap_file.write(data)
-      
 
         else:
             print("Packet has unknown/unsupported family!")
@@ -243,12 +242,10 @@ def ssl_log(app, pcap=None, verbose=False, spawn=False, keylog=False, enable_spa
         if live and p["contentType"] == "datalog":
             try:
                 log_pcap(named_pipe, p["ss_family"], p["ssl_session_id"], p["function"], p["src_addr"],
-                     p["src_port"], p["dst_addr"], p["dst_port"], data)
+                         p["src_port"], p["dst_addr"], p["dst_port"], data)
             except (BrokenPipeError, IOError):
                 process.detach()
                 cleanup(live)
-                
-
 
         if keylog and p["contentType"] == "keylog":
             keylog_file.write(p["keylog"] + "\n")
@@ -290,27 +287,22 @@ def ssl_log(app, pcap=None, verbose=False, spawn=False, keylog=False, enable_spa
     else:
         process = device.attach(app)
 
-
-
-        
-
     if live:
         if pcap:
             print("[*] YOU ARE TRYING TO WRITE A PCAP AND HAVING A LIVE VIEW\nTHIS IS NOT SUPPORTED!\nWHEN YOU DO A LIVE VIEW YOU CAN SAFE YOUR CAPUTRE WIHT WIRESHARK.")
         fifo_file = temp_fifo()
         print(f'[*] friTap live view on Wireshark')
         print(f'[*] Created named pipe for Wireshark live view to {fifo_file}')
-        print(f'[*] Now open this named pipe with Wireshark in another terminal: sudo wireshark -k -i {fifo_file}')
+        print(
+            f'[*] Now open this named pipe with Wireshark in another terminal: sudo wireshark -k -i {fifo_file}')
         print(f'[*] friTap will continue after the named pipe is ready....\n')
-        #input()
+        # input()
         #named_pipe = os.open(fifo_file, os.O_WRONLY | os.O_CREAT | os.O_NONBLOCK)
         named_pipe = open(fifo_file, "wb", 0)
         named_pipe = write_pcap_header(named_pipe)
     elif pcap:
         pcap_file = open(pcap, "wb", 0)
         pcap_file = write_pcap_header(pcap_file)
-
-
 
     if keylog:
         keylog_file = open(keylog, "w")
@@ -322,7 +314,6 @@ def ssl_log(app, pcap=None, verbose=False, spawn=False, keylog=False, enable_spa
         print(f'[*] Logging pcap to {pcap}')
     if keylog:
         print(f'[*] Logging keylog file to {keylog}')
-
 
     if spawn:
         device.resume(pid)
@@ -365,7 +356,7 @@ Examples:
                       const=True, help="Attach to a process on android")
     args.add_argument("-k", "--keylog", metavar="<path>", required=False,
                       help="Log the keys used for tls traffic")
-    args.add_argument("-l", "--live",required=False,action="store_const",const=True,
+    args.add_argument("-l", "--live", required=False, action="store_const", const=True,
                       help="Creates a named pipe /tmp/sharkfin which can be read by Wireshark during the capturing process")
     args.add_argument("-p ", "--pcap", metavar="<path>", required=False,
                       help="Name of PCAP file to write")
@@ -381,6 +372,6 @@ Examples:
 
     try:
         ssl_log(parsed.exec, parsed.pcap, parsed.verbose,
-            parsed.spawn, parsed.keylog, parsed.enable_spawn_gating, parsed.android, parsed.live)
+                parsed.spawn, parsed.keylog, parsed.enable_spawn_gating, parsed.android, parsed.live)
     finally:
         cleanup(parsed.live)
