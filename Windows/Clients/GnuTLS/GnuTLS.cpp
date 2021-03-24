be6775c7c167c51b22c918f3313d84a4797394e3
Francois Egner <francois.egner@fkie.fraunhofer.de>
2021-03-24 11:23:37 +0100

    Cleaned up GnuTLS client code and send/receive lopp implemented

--- Windows/Clients/GnuTLS/GnuTLS.cpp
+++ Windows/Clients/GnuTLS/GnuTLS.cpp
@@ -1,13 +1,15 @@
 #include "GnuTLS.h"
 
-
-
 void GNUTLS_init() {
 	gnutls_global_init();
 }
 
-void GNUTLS_cleanup() {
-	gnutls_global_deinit();
+void GNUTLS_cleanup(GNUTLS_Connection* connection) {
+    
+    gnutls_deinit(connection->session);
+    gnutls_certificate_free_credentials(connection->xcred);
+    gnutls_global_deinit();
+    free(connection);
 }
 
 int tcp_connect(const char* hostname, int port){
@@ -30,44 +32,85 @@ int tcp_connect(const char* hostname, int port){
 
 void GNUTLS_setup_and_connect(GNUTLS_Connection* connection, const char* hostname, int port) {
     const char* errptr = NULL;
-	gnutls_session_t session;
     WSADATA wsa;
     int result;
-    gnutls_certificate_credentials_t xcred;
-
-    gnutls_certificate_allocate_credentials(&xcred);
-    gnutls_certificate_set_x509_system_trust(xcred);
 
     if (WSAStartup(MAKEWORD(2, 2), &wsa) != 0) {
         printf("Failed to initialize WSA. Error: %d\n", WSAGetLastError());
     }
 
-    
+    gnutls_certificate_allocate_credentials(&connection->xcred);
+    gnutls_certificate_set_x509_system_trust(connection->xcred);
 
-    result = gnutls_init(&session, GNUTLS_CLIENT);
-    printf("GNUTLS_INIT: %d\n", result);
+    result = gnutls_init(&connection->session, GNUTLS_CLIENT);
 
-    result = gnutls_set_default_priority(session);
-    printf("GNUTLS_PRIORITY_DEFAULT: %d\n", result);
+    result = gnutls_set_default_priority(connection->session);
 
-    gnutls_credentials_set(session, GNUTLS_CRD_CERTIFICATE, xcred);
+    gnutls_credentials_set(connection->session, GNUTLS_CRD_CERTIFICATE, connection->xcred);
 
     connection->socket = tcp_connect(hostname, port);
    
-    gnutls_transport_set_int(session, connection->socket);
-    gnutls_handshake_set_timeout(session,
-        GNUTLS_DEFAULT_HANDSHAKE_TIMEOUT);
+    gnutls_transport_set_int(connection->session, connection->socket);
+    gnutls_handshake_set_timeout(connection->session, GNUTLS_DEFAULT_HANDSHAKE_TIMEOUT);
 
-    gnutls_handshake(session);
-    gnutls_record_send(session, "Ya yeet!", strlen("Ya yeet!"));
+    gnutls_handshake(connection->session);
+    
+}
+
+char* GNUTLS_get_response(GNUTLS_Connection* connection) {
+
+    unsigned long responseBufferSize = 0;
+    char* responseBuffer = (char*)malloc(1);
+    if (responseBuffer == NULL) printf("Allocating space for responseBuffer failed!");
+    responseBuffer[0] = '\0';
+
+    while (1) {
+        //Neuen temporären Buffer erstellen
+        char* tempBuffer = (char*)malloc(TMP_BUFFER_SIZE);
+        if (tempBuffer == NULL) printf("Couldnt allocate memory for tempBuffer...");
+
+        int readBytesCount = gnutls_record_recv(connection->session, tempBuffer, TMP_BUFFER_SIZE);
+
+
+        if (readBytesCount <= 0) {
+            //Wenn keine Daten gelesen wurden, dann hänge hinten ein nullterm-Char dran und gebe reponseBuffer-Pointer zurück
+            char* returnBuffer = (char*)realloc(responseBuffer, responseBufferSize + 1);
+            if (returnBuffer == NULL) printf("Couldnt realloc memory for returnBuffer...");
+            returnBuffer[responseBufferSize] = '\0';
+            return returnBuffer;
+        }
+
+        //Wenn neue Daten gelesen wurden, dann erweitere ResponseBuffer und schreibe neue Daten hinein
+        char* tempResponseBuffer = (char*)realloc(responseBuffer, responseBufferSize + readBytesCount);
+        if (tempResponseBuffer == NULL) printf("Couldnt realloc memory for tempResponseBuffer...");
+        responseBuffer = tempResponseBuffer;
+        memcpy(responseBuffer + responseBufferSize, tempBuffer, readBytesCount);
+        responseBufferSize += readBytesCount;
+        free(tempBuffer);
+
+    }
+}
+
+void GNUTLS_run() {
+    printf("GnuTLS %s\n-------------------\n", GNUTLS_VERSION);
+    GNUTLS_init();
+
+    GNUTLS_Connection* connection = (GNUTLS_Connection*)malloc(sizeof(GNUTLS_Connection));
+    while (1) {
+        GNUTLS_setup_and_connect(connection, HOSTNAME, 443);
+        gnutls_record_send(connection->session, MSG, strlen(MSG));
+        char* response = GNUTLS_get_response(connection);
+        printf("%s\n", response);
+        Sleep(3000);
+    }
+
+    GNUTLS_cleanup(connection);
 }
 
 int main() {
 
-    GNUTLS_Connection* connection = (GNUTLS_Connection*)malloc(sizeof(GNUTLS_Connection));
+    GNUTLS_run();
 
-    GNUTLS_init();
-    GNUTLS_setup_and_connect(connection, HOSTNAME, 443);
-    GNUTLS_cleanup();
+   
 
 }
--- Windows/Clients/GnuTLS/GnuTLS.h
+++ Windows/Clients/GnuTLS/GnuTLS.h
@@ -5,12 +5,18 @@
 #pragma warning(suppress : 4996)
 #define CHECK(x) assert((x)>=0)
 #define HOSTNAME "127.0.0.1"
-
+#define GNUTLS_VERSION "6.3.0"
+#define TMP_BUFFER_SIZE 1024
+#define MSG "Ya yeet!"
 
 typedef struct GNUTLS_Connection {
     int socket;
+    gnutls_session_t session;
+    gnutls_certificate_credentials_t xcred;
 }GNUTLS_Connection;
 
+int tcp_connect(const char* ip, int port);
 void GNUTLS_init();
 void GNUTLS_cleanup();
+void GNUTLS_run();
 void GNUTLS_setup_and_connect(GNUTLS_Connection* connection, const char* hostname, int port);
