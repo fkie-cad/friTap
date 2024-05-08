c3c56346ff1eff18a956607511700a838720c7b7
lengersdorffj0 <s6juleng@uni-bonn.de>
2024-05-08 11:37:02 +0200

    s2ntls_linux.ts

--- agent/linux/s2ntls_linux.ts
+++ agent/linux/s2ntls_linux.ts
@@ -1,3 +1,4 @@
+import { socket_library } from "../../friTap/_ssl_log_legacy";
 import { S2nTLS } from "../ssl_lib/s2ntls";
 
 export class S2nTLS_Linux extends S2nTLS{
@@ -11,4 +12,11 @@ export class S2nTLS_Linux extends S2nTLS{
         this.install_plaintext_write_hook();
         this.install_tls_keys_callback_hook();
     }
+
+    //implement key callback hook
+}
+
+export function s2ntls_execute(moduleName: String){
+    var s2n_tls = new S2nTLS_Linux(moduleName, socket_library);
+    s2n_tls.execute_hooks();
 }
