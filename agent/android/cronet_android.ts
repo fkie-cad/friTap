bbd44ce993938e052730bbb13320162d9b58f38a
Daniel Baier <admin@remoteshell-security.com>
2025-07-24 14:23:26 +0200

    some fixes in the hooking for newer version of libcronet on Android systems

--- agent/android/cronet_android.ts
+++ agent/android/cronet_android.ts
@@ -5,11 +5,30 @@ import {PatternBasedHooking, get_CPU_specific_pattern } from "../shared/pattern_
 import { patterns, isPatternReplaced } from "../ssl_log.js"
 import { devlog } from "../util/log.js";
 
+const EXCLUDED_MODULE_SUFFIXES = ["_libpki.so", "_libcrypto.so", "libmainlinecronet.136.0.7091.2.so"]; // extensible list
+
+interface PatternSet {
+  primary:  string;
+  fallback: string;
+  second_fallback?: string;
+}
+
+interface Patterns {
+  [arch: string]: PatternSet;
+}
+
+const STABLE_CRONET_PATTERNS = {
+    "arm64": {
+        primary:  "FF 83 02 D1 FD 7B 05 A9 F9 33 00 F9 F8 5F 07 A9 F6 57 08 A9 F4 4F 09 A9 FD 43 01 91 58 D0 3B D5 08 17 40 F9 A8 83 1F F8 08 34 40 F9 08 21 41 F9 28 11", // Primary pattern
+        fallback: "3F 23 03 D5 FF ?3 02 D1 FD 7B 0? A9 F? ?? 0? ?9 F6 57 0? A9 F4 4F 0? A9 FD ?3 01 91 08 34 40 F9 08 ?? 41 F9 ?8 ?? 00 B4" // Fallback pattern
+    }
+};
+
 
 export class Cronet_Android extends Cronet {
     private default_pattern: { [arch: string]: { primary: string; fallback: string, second_fallback?: string; } };
 
-    constructor(public moduleName:string, public socket_library:String, is_base_hook: boolean){
+    constructor(public moduleName:string, public socket_library:String, is_base_hook: boolean, patterns?: Patterns){
         super(moduleName,socket_library,is_base_hook);
 
         this.default_pattern = {
@@ -33,6 +52,11 @@ export class Cronet_Android extends Cronet {
                 fallback: "2D E9 F0 41 86 B0 04 46 40 6B D0 F8 30 01 00 28 53 D0"  // Fallback pattern
             }
         };
+
+        if(patterns){
+            // If patterns are provided, use them instead of the default ones
+            this.default_pattern = patterns;
+        }
     }
 
     getSoName(modulePath: string): string {
@@ -122,7 +146,19 @@ export class Cronet_Android extends Cronet {
 
 
 export function cronet_execute(moduleName:string, is_base_hook: boolean){
-    let cronet = new Cronet_Android(moduleName,socket_library,is_base_hook);
+    // Check if the moduleName ends with any of the excluded suffixes
+    if (EXCLUDED_MODULE_SUFFIXES.some(suffix => moduleName.endsWith(suffix))) {
+        devlog(`[-] Skipping module ${moduleName} due to excluded suffix.`);
+        return;
+    }
+    
+    let cronet: Cronet_Android;
+    if(moduleName.startsWith("stable_cronet")){
+        cronet = new Cronet_Android(moduleName,socket_library,is_base_hook, STABLE_CRONET_PATTERNS);
+    }else{
+        cronet = new Cronet_Android(moduleName,socket_library,is_base_hook);
+    }
+    
     try {
         let hooker = cronet.execute_hooks();
         // wait 1 sec before we continue
